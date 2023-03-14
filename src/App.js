4ecf97e81520e239bfc4a30f3f8a7237ac53c608
SypFilter <gastomafo@gmail.com>
2023-03-14 16:55:03 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import {
+  Container,
+  Header,
+  LogoDiv,
+  Navbar,
+  Links,
+  Fild,
+  SearchSection,
+  FildDiv,
+  Button,
+} from "./components/styled-components.jsx";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [weatherData, SetWeatherData] = useState("");
+
+  const informations = {
+    methods: "GET",
+    headers: {
+      "X-RapidAPI-Key": "7b77c9deed1b4a17937192541230602",
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+  };
+
+  function fetchWeather() {
+    if (value === "") {
+      alert("Put the correct place!");
+      return;
+    }
+
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=7b77c9deed1b4a17937192541230602&q=${value}&aqi=yes`,
+      informations
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Place don't exist");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        SetWeatherData(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Place don't exist");
+      });
+  }
+
+  console.log(value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <LogoDiv>
+          <span className="header-main">Weather.Filter( )</span>
+        </LogoDiv>
+        <Navbar>
+          <Links href="#">Home</Links>
+          <Links href="https://github.com/SypFilter">Github</Links>
+          <Links href="#">About</Links>
+        </Navbar>
+      </Header>
+      <SearchSection>
+        <span className="search-text">Search Here</span>
+        <FildDiv>
+          <Fild
+            onBlur={(e) => setValue(e.target.value)}
+            placeholder="The place you want here!"
+          />
+        </FildDiv>
+        <Button onClick={() => fetchWeather()}>🔍</Button>
+      </SearchSection>
+      <div className="dates-wrapper">
+        {weatherData.location && (
+          <div className="dates-wrapper">
+            <p className="city">City: {weatherData.location.name}</p>
+            <div className="display-fix">
+              <div className="img-position">
+                <img
+                  src={weatherData.current.condition.icon}
+                  alt="Weather condition"
+                />
+                <p className="temperature">{weatherData.current.temp_c}°C</p>
+              </div>
+            </div>
+            <div className="float">
+              <p className="condition">{weatherData.current.condition.text}</p>
+              <p className="humidity">
+                Humidity: {weatherData.current.humidity}%
+              </p>
+              <p className="wind">Wind: {weatherData.current.wind_kph} km/h</p>
+              <p className="pressure">
+                Pressure: {weatherData.current.pressure_mb} mb
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    </Container>
   );
 }
 
---
+++ src/components/styled-components.jsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+const Container = styled.body `
+    width: 100%;
+    height: 100%;
+`
+const Header = styled.div `
+    display: flex;
+    background-color: #000;
+    color: white; 
+    height: 57px
+`
+
+const LogoDiv = styled.div `
+    padding: 13px;
+    padding-left: 40px
+`
+
+const Navbar = styled.div `
+    display: flex;
+    padding: 18px;
+    padding-left: 30px;
+`
+
+const Links = styled.a `
+    cursor: pointer;
+    text-decoration: none;
+    color: white;
+    font-size: 20px;
+    font-weight: lighter;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    padding-left: 40px;
+    &:hover { 
+        transition: 0.5s;
+        color: #808080e5;
+    }
+`
+
+const SearchSection = styled.div `
+     display: flex;
+     justify-content: center;
+     width: 100%;
+     height: 50px;
+     background-color: #4040edd0;
+`
+
+const FildDiv = styled.div `
+    margin-left: 20px;
+`
+
+const Fild = styled.input `
+    margin-top: 6px;
+    border: none;
+    border-radius: 20px;
+    padding: 10px;
+    padding-right: 72vw;
+    background-color: #333;
+    color: white;
+`
+
+const Button = styled.button `
+    display: flex;
+    justify-content: end;
+    padding:2px;
+    margin-top: 12px;
+    margin-left: 1vw;
+    margin-block-end: 15px;
+`
+export {Container, Header, LogoDiv, Navbar, Links, Fild, SearchSection, FildDiv, Button};
